24118093519f2e0a199849e4cb17a8514ec210c8
Kabilan V <120373629+Kabilanvel22@users.noreply.github.com>
2024-03-19 15:39:35 +0530

    first-commit

--- Day2/src/pages/Emicalculator.jsx
+++ Day2/src/pages/Emicalculator.jsx
@@ -31,16 +31,7 @@ function EMICalculator() {
             />
             <p>{loanAmount}</p>
           </div>
-          <div className="mb-4 ml-20">
-            <label className="block text-sm font-medium text-gray-700">Interest Rate (%)</label>
-            <Slider
-              axis="x"
-              x={interestRate}
-              xmin={0}
-              xmax={20}
-              onChange={({ x }) => setInterestRate(x)}
-            />
-            <p>{interestRate}</p>
+          terestRate}</p>
           </div>
           <div className="mb-4 ml-20">
             <label className="block text-sm font-medium text-gray-700">Loan Tenure (years)</label>
@@ -51,33 +42,7 @@ function EMICalculator() {
               xmax={30}
               onChange={({ x }) => setLoanTenure(x)}
             />
-            <p>{loanTenure}</p>
-          </div>
-          <button
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-20"
-            onClick={calculateEmi}
-          >
-            Calculate EMI
-          </button>
-        </div>
-        <div className="bg-white p-4 rounded-lg shadow-md">
-          <div>
-          <h2 className='mt-14 mb-5 font-serif'>Estimated Amount Required</h2>
-            {emi ? (
-              <>
-                <p className="text-lg ml-36 font-bold text-4xl"> ₹ {emi}</p>
-                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4 ml-44">
-                  Apply
-                </button>
-              </>
-            ) : ( 
-              <p className="text-lg ml-32">₹ 0 <br/> Please calculate EMI</p>
-            )}
-            <p class='mt-10 ml-5 text-gray-500'>*This is an approximate EMI calculation. The actual processing fee and rate of interest may vary. Interest rate for Personal Loan starts at 2% on a monthly basis</p>
-          </div>
-        </div>
-      </div>
-    </div>
+           
   );
 }
 
