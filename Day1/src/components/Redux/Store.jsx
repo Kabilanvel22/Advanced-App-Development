78ef382fb92a49c536105238e5fcb49dff350c7f
Kabilan V <120373629+Kabilanvel22@users.noreply.github.com>
2024-03-19 09:17:50 +0530

    Day1/src directory

--- Day1/src/components/Redux/Store.jsx
+++
@@ -1,7 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import userReducer from './Userslice'
-export default configureStore({
-    reducer :{
-        user:userReducer,
-    }
-});
--- Day1/src/pages/Aboutloan.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Aboutloan = () => {
-  return (
-    <div>loan details</div>
-  )
-}
-
-export default Aboutloan
